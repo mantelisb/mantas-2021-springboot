70aeda4aea9d0dad5f3d2427a7d14f22c4242542
mantelisb <mantas.boronilscikovas@gmail.com>
2021-02-11 16:05:41 +0200

    fix: tests

--- src/test/java/com/example/demo/MessageControllerTests.java
+++ src/test/java/com/example/demo/MessageControllerTests.java
@@ -30,11 +30,11 @@ public class MessageControllerTests {
     @Test
     public void shouldBEandDBWork() {
         when(repository.findAll()).thenReturn(Arrays.asList(new Message(MESSAGE)));
-        assertEquals(MessageController.BE_SUCCESS_MESSAGE + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.getMessage());
+        assertEquals(MessageController.BE_SUCCESS_MESSAGE + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.getMessages());
     }
     @Test
     public void shouldWorkOnlyBE() {
         when(repository.findAll()).thenReturn(new ArrayList<>());
-        assertEquals(MessageController.BE_SUCCESS_MESSAGE, controller.getMessage());
+        assertEquals(MessageController.BE_SUCCESS_MESSAGE, controller.getMessages());
     }
 }
